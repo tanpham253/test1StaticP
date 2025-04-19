aadf4a9012aa63b3914355ef4a2616265cdfe0a4
tanpham253 <85584571+tanpham253@users.noreply.github.com>
2025-04-19 17:19:01 +0700

    temp main page

--- ComposedScript.js
+++ ComposedScript.js
@@ -26,7 +26,8 @@ getLocation();
 //inject tags dirrectly
 let dateTime = document.getElementById("dateTime");
 let date = new Date();
-
+var x;
+if (date.getHours()> 12){x='PM'}else {x='AM'}
   dateTime.innerHTML =
     "Date: " +
     date.getDate() +
@@ -35,4 +36,22 @@ let date = new Date();
     "/" +
     date.getFullYear() +
     " Time: " +
-    date.getHours();
+    date.getHours() +' '+ x;
+//slide show module, source: https://www.w3schools.com/w3css/w3css_slideshow.asp
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides1");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
